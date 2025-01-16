8f110aae6a0a50bd0451c2d44f15ea1bd745cf7a
Cleef <cleefgulfan312@gmail.com>
2025-01-16 00:55:48 +0000

    Created CreateMenu() function, moved from Main().

--- TestBackend_OldStyle/Program.cs
+++ TestBackend_OldStyle/Program.cs
@@ -6,14 +6,17 @@ class Program
     {
         Console.WriteLine("This is a test for developing the backend for an online vehicle Service Book");
         
+        CreateMenu();
+        
+        int choice = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(choice);
+    }
+
+    static void CreateMenu()
+    {
         Console.WriteLine("Menu:");
         Console.WriteLine("1) View");
         Console.WriteLine("2) Add");
         Console.WriteLine("3) Edit");
-        
-        int choice = Convert.ToInt32(Console.ReadLine());
-        
-        Console.WriteLine(choice);
-        
     }
 }
