b1d76804f00ca720c428de3540c03901f5f3fab3
Cleef <cleefgulfan312@gmail.com>
2025-01-16 00:54:10 +0000

    Created basic input.

--- TestBackend_OldStyle/Program.cs
+++ TestBackend_OldStyle/Program.cs
@@ -10,5 +10,10 @@ class Program
         Console.WriteLine("1) View");
         Console.WriteLine("2) Add");
         Console.WriteLine("3) Edit");
+        
+        int choice = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine(choice);
+        
     }
 }
