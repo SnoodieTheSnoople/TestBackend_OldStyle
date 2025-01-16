0e4ef608e296ef35a9425ef25c65efed8afc43ae
Cleef <cleefgulfan312@gmail.com>
2025-01-16 00:51:47 +0000

    Created main menu.

---
+++ TestBackend_OldStyle/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TestBackend_OldStyle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("This is a test for developing the backend for an online vehicle Service Book");
+        
+        Console.WriteLine("Menu:");
+        Console.WriteLine("1) View");
+        Console.WriteLine("2) Add");
+        Console.WriteLine("3) Edit");
+    }
+}
